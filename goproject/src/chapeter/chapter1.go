a8f10f56aa2980739135fa1a4f1bdfeed1d7d6e5
pangluo <59036386@qq.com>
2019-11-17 13:46:11 +0800

    commit
    variableUse

--- goproject/src/chapeter/chapter1.go
+++ goproject/src/chapeter/chapter1.go
@@ -3,6 +3,7 @@ package main
 import "fmt" //ftm 包中提供格式化
 
 func main () {
+	//要求：请使用一句输出语句，达到如下图形的效果
 	fmt.Println("姓名\t年龄\t籍贯\t住址\njohn\t12\t河北\t北京")
 }
 
---
+++ goproject/src/chapter2/variableUse.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+//变量的使用 - 快速入门
+func main(){
+
+    var i int
+    i = 10
+    fmt.Println("",i)
+}
