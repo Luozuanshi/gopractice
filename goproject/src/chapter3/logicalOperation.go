45a02326fcb37bcbba1e388a60e35f4d71d0f7a5
pangluo <59036386@qq.com>
2019-11-20 13:48:30 +0800

    关系运算之逻辑运算的短路与短路或

--- goproject/src/chapter3/logicalOperation.go
+++ goproject/src/chapter3/logicalOperation.go
@@ -5,8 +5,35 @@ import (
 	_ "time"
 )
 //关系运算符之逻辑运算的使用
+
+func test() bool {
+	fmt.Println("TEST...")
+	return true
+}
 func main () {
+	var v int = 10
+	//演示短路与
+	// v > 9 为true,test() 为true 因此先输出TEST...后输出ok...
+	if v > 9 && test() {
+		fmt.Println("ok...") 
+	}
+	// v > 11 为false,不判断后面的 test() 因此不输出
+	if v > 11 && test() {
+		fmt.Println("ok...") 
+	}
+	//演示短路或 
+	//v > 9 为true 直接不继续判断 test() 因此直接输出 pk...
+	if v > 9 || test() {
+		fmt.Println("pk...") 
+	}
+	//v > 11 为false 继续判断 test() 因此先输出TEST...后输出pk...
+	if v > 11 || test() {
+		fmt.Println("pk...") 
+	}
+
 	
+
+
 	//演示逻辑 && 操作
 	var age int =40
 
